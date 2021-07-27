da928e55e5f51c14da781ebef6386ef82fac9450
Ehsan khaleghian <e.khaleghian@gmail.com>
2021-07-27 09:39:43 +0430

    clear

--- javascript/practice2.js
+++ javascript/practice2.js
@@ -22,23 +22,19 @@ const resultHandler = () => {
 // }
 // console.log("ENTERIES:::::", ids.entries());
 
-const person1={name: "Ehsan"}
-const person2={name:"Mahdi"}
-const person3={name:"jafar"}
+const person1 = { name: "Ehsan" };
+const person2 = { name: "Mahdi" };
+const person3 = { name: "jafar" };
 
-const personData=new Map([[person1,{age:34,gender:"male"}],[person2,{age:21,gender: "male"}]])
-personData.set(person3, [{age:120, gender:"dinoser"}, "10,000 BC"])
+const personData = new Map([
+    [person1, { age: 34, gender: "male" }],
+    [person2, { age: 21, gender: "male" }],
+]);
+personData.set(person3, [{ age: 120, gender: "dinoser" }, "10,000 BC"]);
 
-console.log("Person Data Get::::",personData.get(person3))
+console.log("Person Data Get::::", personData.get(person3));
 
-for (const [key,value] of personData.entries()){
-    console.log("Key::::",key)
-    console.log("Value::::",value)
+for (const [key, value] of personData.entries()) {
+    console.log("Key::::", key);
+    console.log("Value::::", value);
 }
-
-
-
-
-
-
-
